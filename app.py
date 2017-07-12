ed46e1bf8313b81c4075283c7f1cf1f67dba484f
mrjoness <mrjoness@umich.edu>
2017-07-12 14:47:59 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.secret_key = '/x0e!/xc2/xf0/xcc/x98/xc0/xdaF/x1b/x13/xd5/x08I/xfa/xa6/xbcqn/xd3t/x00G/xf4'
